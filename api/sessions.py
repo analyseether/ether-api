0c2bd47f0173410a3d80c161b3eb26029e3000e2
SaranshVatsa <saranshvatsa9@gmail.com>
2018-09-19 21:06:28 +0530

    Shifted to SQLAlchemy for queries, added links

---
+++ api/sessions.py
@@ -0,0 +1,33 @@
+from sqlalchemy import MetaData, create_engine
+from flask import Flask
+from sqlalchemy.orm import sessionmaker
+
+if __package__ is None or __package__ == '':
+  from settings import DefaultSettings
+else:
+  from .settings import DefaultSettings
+
+app = Flask(__name__)
+app.config.from_object(DefaultSettings)
+
+user= str(app.config['POSTGRES_USER'])
+password= str(app.config['POSTGRES_PASSWORD'])
+host=str(app.config['POSTGRES_HOST'])
+port=str(app.config['POSTGRES_PORT'])
+db=str(app.config['POSTGRES_DB'])
+
+engine = create_engine('postgresql://' + user +':' + password +'@' + host +  ':' + port + '/' + db)
+
+
+class Session():
+    def __init__(self):
+        self.engine = create_engine('postgresql://' + user +':' + password +'@' + host +  ':' + port + '/' + db)
+
+    def connect_to_psql(self):
+        Ses = sessionmaker(bind=self.engine)
+        return Ses()
+
+    def get_table_object(self, name):
+        META_DATA = MetaData(bind=self.engine, reflect=True)
+
+        return META_DATA.tables[name]
